4dd58b3e8f1cd0feeead47bce21b8fb1dd3b9123
Daniel Cross <crossy1686@gmail.com>
2019-05-20 22:07:18 +0200

    added math equations for lifts

--- src/components/deadlift.js
+++ src/components/deadlift.js
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Deadlift = () => {
+	const [ name, setName ] = useState('');
+
 	return (
 		<div id="Deadlift">
-			<form action="submit" onSubmit="run">
-				<label htmlFor="Enter 1PM">Enter your one rep max:
-				<input type="number" placeholder="Weight in KG"/>
+			<form action="submit">
+				<label htmlFor="Enter 1PM">
+					Enter your one rep max:
+					<input
+						type="number"
+						placeholder="Weight in KG"
+						onChange={(e) => setName(e.target.value)}
+						value={name}
+					/>
 				</label>
-				<button>Submit</button>
 			</form>
-			<h2>Deadlift: 0</h2>
+			<h2>Deadlift</h2>
+			<div className="week">
+				<h3>Week 1</h3>
+				<p>Warmup: {name * 0.2}</p>
+				<p>Warmup: {name * 0.3}</p>
+				<p>Warmup: {name * 0.5}</p>
+				<p>Set 1 - 5 Reps: {name * 0.6}</p>
+				<p>Set 2 - 3 Reps: {name * 0.7}</p>
+				<p>Set 3 - 1 Rep: {name * 0.8}</p>
+			</div>
+			<div className="week">
+				<h3>Week 2 - 3 Lifts per set</h3>
+				<p>Warmup: {name * 0.2}</p>
+				<p>Warmup: {name * 0.3}</p>
+				<p>Warmup: {name * 0.5}</p>
+				<p>Set 1 - 5 Reps: {name * 0.65}</p>
+				<p>Set 2 - 3 Reps: {name * 0.75}</p>
+				<p>Set 3 - 1 Rep: {name * 0.85}</p>
+			</div>
+			<div className="week">
+				<h3>Week 3 - 1 Lift per set</h3>
+				<p>Warmup: {name * 0.2}</p>
+				<p>Warmup: {name * 0.3}</p>
+				<p>Warmup: {name * 0.5}</p>
+				<p>Set 1 - 5 Reps: {name * 0.8}</p>
+				<p>Set 2 - 3 Reps: {name * 0.85}</p>
+				<p>Set 3 - 1 Rep: {name * 0.9}</p>
+			</div>
+			<div className="week">
+				<h3>Week 4 - Deload Week</h3>
+				<p>Warmup: {name * 0.2}</p>
+				<p>Warmup: {name * 0.3}</p>
+				<p>Warmup: {name * 0.4}</p>
+				<p>Set 1 - 5 Reps: {name * 0.5}</p>
+				<p>Set 2 - 3 Reps: {name * (0.55).toFixed(1)}</p>
+				<p>Set 3 - 1 Rep: {name * 0.6}</p>
+			</div>
 		</div>
 	);
 };
--- src/components/squat.js
+++ src/components/squat.js
@@ -1,16 +1,11 @@
-import React from 'react';
-import { useWeightInput } from '../Hooks/useWeightInput';
+import React, { useState } from 'react';
 
 const Squat = () => {
-	const [ name, setName ] = useWeightInput('');
+	const [ name, setName ] = useState('');
 
 	return (
 		<div id="Squat">
-			<form
-				action="submit"
-				onSubmit={(e) => {
-					e.preventDefault();
-				}}>
+			<form action="submit">
 				<label htmlFor="Enter 1PM">
 					Enter your one rep max:
 					<input
@@ -20,9 +15,44 @@ const Squat = () => {
 						value={name}
 					/>
 				</label>
-				<button>Submit</button>
 			</form>
-			<h2>Squat: 0</h2>
+			<h2>Squat</h2>
+			<div className="week">
+				<h3>Week 1</h3>
+				<p>Warmup: {name * 0.2}</p>
+				<p>Warmup: {name * 0.3}</p>
+				<p>Warmup: {name * 0.5}</p>
+				<p>Set 1 - 5 Reps: {name * 0.6}</p>
+				<p>Set 2 - 3 Reps: {name * 0.7}</p>
+				<p>Set 3 - 1 Rep: {name * 0.8}</p>
+			</div>
+			<div className="week">
+				<h3>Week 2 - 3 Lifts per set</h3>
+				<p>Warmup: {name * 0.2}</p>
+				<p>Warmup: {name * 0.3}</p>
+				<p>Warmup: {name * 0.5}</p>
+				<p>Set 1 - 5 Reps: {name * 0.65}</p>
+				<p>Set 2 - 3 Reps: {name * 0.75}</p>
+				<p>Set 3 - 1 Rep: {name * 0.85}</p>
+			</div>
+			<div className="week">
+				<h3>Week 3 - 1 Lift per set</h3>
+				<p>Warmup: {name * 0.2}</p>
+				<p>Warmup: {name * 0.3}</p>
+				<p>Warmup: {name * 0.5}</p>
+				<p>Set 1 - 5 Reps: {name * 0.8}</p>
+				<p>Set 2 - 3 Reps: {name * 0.85}</p>
+				<p>Set 3 - 1 Rep: {name * 0.9}</p>
+			</div>
+			<div className="week">
+				<h3>Week 4 - Deload Week</h3>
+				<p>Warmup: {name * 0.2}</p>
+				<p>Warmup: {name * 0.3}</p>
+				<p>Warmup: {name * 0.4}</p>
+				<p>Set 1 - 5 Reps: {name * 0.5}</p>
+				<p>Set 2 - 3 Reps: {name * (0.55).toFixed(1)}</p>
+				<p>Set 3 - 1 Rep: {name * 0.6}</p>
+			</div>
 		</div>
 	);
 };
