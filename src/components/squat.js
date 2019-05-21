d9fe733e7b84a6c3c629f176cd986a69068e6b52
Daniel Cross <crossy1686@gmail.com>
2019-05-21 08:18:58 +0200

    updated weight equations

--- src/components/squat.js
+++ src/components/squat.js
@@ -22,36 +22,36 @@ const Squat = () => {
 				<p>Warmup: {name * 0.2}</p>
 				<p>Warmup: {name * 0.3}</p>
 				<p>Warmup: {name * 0.5}</p>
-				<p>Set 1 - 5 Reps: {name * 0.6}</p>
-				<p>Set 2 - 3 Reps: {name * 0.7}</p>
-				<p>Set 3 - 1 Rep: {name * 0.8}</p>
+				<p>5 Reps: {name * 0.6}</p>
+				<p>3 Reps: {name * 0.7}</p>
+				<p>1 Rep: {name * 0.8}</p>
 			</div>
 			<div className="week">
 				<h3>Week 2 - 3 Lifts per set</h3>
 				<p>Warmup: {name * 0.2}</p>
 				<p>Warmup: {name * 0.3}</p>
 				<p>Warmup: {name * 0.5}</p>
-				<p>Set 1 - 5 Reps: {name * 0.65}</p>
-				<p>Set 2 - 3 Reps: {name * 0.75}</p>
-				<p>Set 3 - 1 Rep: {name * 0.85}</p>
+				<p>5 Reps: {name * 0.65}</p>
+				<p>3 Reps: {name * 0.75}</p>
+				<p>1 Rep: {name * 0.85}</p>
 			</div>
 			<div className="week">
 				<h3>Week 3 - 1 Lift per set</h3>
 				<p>Warmup: {name * 0.2}</p>
 				<p>Warmup: {name * 0.3}</p>
 				<p>Warmup: {name * 0.5}</p>
-				<p>Set 1 - 5 Reps: {name * 0.8}</p>
-				<p>Set 2 - 3 Reps: {name * 0.85}</p>
-				<p>Set 3 - 1 Rep: {name * 0.9}</p>
+				<p>5 Reps: {name * 0.8}</p>
+				<p>3 Reps: {name * 0.85}</p>
+				<p>1 Rep: {name * 0.9}</p>
 			</div>
 			<div className="week">
 				<h3>Week 4 - Deload Week</h3>
 				<p>Warmup: {name * 0.2}</p>
 				<p>Warmup: {name * 0.3}</p>
 				<p>Warmup: {name * 0.4}</p>
-				<p>Set 1 - 5 Reps: {name * 0.5}</p>
-				<p>Set 2 - 3 Reps: {name * (0.55).toFixed(1)}</p>
-				<p>Set 3 - 1 Rep: {name * 0.6}</p>
+				<p>5 Reps: {name * 0.5}</p>
+				<p>3 Reps: {name * (0.55).toFixed(1)}</p>
+				<p>1 Rep: {name * 0.6}</p>
 			</div>
 		</div>
 	);
